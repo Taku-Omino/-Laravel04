a6764cad17785c240c8b741884cf1498f2986330
Taku-Omino <47404544+Taku-Omino@users.noreply.github.com>
2023-04-28 21:46:09 +0900

    2023/04/28 05commit

--- resources/views/front/page/book/create.blade.php
+++ resources/views/front/page/book/create.blade.php
@@ -3,22 +3,23 @@
 @section('title', 'book')
 
 @section('main')
-<h1>著書登録</h1>
+<h1 class="page-name">著書登録ページです。</h1>
+<h2>著書登録</h2>
 
 {!! Form::open(['route' => 'books.store']) !!}
     <div class="form-group">
-        {!! Form::label('title', 'タイトル:') !!}
-        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+        {!! Form::label('title', '著書名') !!}
+        {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'タイトルを入力してください']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('author_id', '著者名:') !!}
+        {!! Form::label('author_id', '著者名') !!}
         {!! Form::select('author_id', $authors, null, ['class' => 'form-control']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('comment', 'コメント:') !!}
-        {!! Form::textarea('comment', null, ['class' => 'form-control']) !!}
+        {!! Form::label('comment', 'コメント') !!}
+        {!! Form::textarea('comment', null, ['class' => 'form-control' , 'placeholder' => '本文を入力してください']) !!}
     </div>
 
     {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
--- resources/views/front/page/book/edit.blade.php
+++ resources/views/front/page/book/edit.blade.php
@@ -3,26 +3,31 @@
 @section('title', 'book')
 
 @section('main')
+<h1 class="page-name">著書編集ページです。</h1>
 <h1>著書編集</h1>
 
-{!! Form::model($book, ['route' => ['books.update', $book->id], 'method' => 'PUT']) !!}
-    <div class="form-group">
-        {!! Form::label('title', 'タイトル:') !!}
-        {!! Form::text('title', null, ['class' => 'form-control']) !!}
-    </div>
+<div class="form-container">
+    {!! Form::model($book, ['route' => ['books.update', $book->id], 'method' => 'PUT']) !!}
+        <div class="form-group">
+            {!! Form::label('title', 'タイトル:', ['class' => 'label']) !!}
+            {!! Form::text('title', null, ['class' => 'input']) !!}
+        </div>
 
-    <div class="form-group">
-        {!! Form::label('author_id', '著者名:') !!}
-        {!! Form::select('author_id', $authors, null, ['class' => 'form-control']) !!}
-    </div>
+        <div class="form-group">
+            {!! Form::label('author_id', '著者名:', ['class' => 'label']) !!}
+            {!! Form::select('author_id', $authors, null, ['class' => 'select']) !!}
+        </div>
 
-    <div class="form-group">
-        {!! Form::label('comment', 'コメント:') !!}
-        {!! Form::textarea('comment', null, ['class' => 'form-control']) !!}
-    </div>
+        <div class="form-group">
+            {!! Form::label('comment', 'コメント:', ['class' => 'label']) !!}
+            {!! Form::textarea('comment', null, ['class' => 'textarea']) !!}
+        </div>
 
-    {!! Form::submit('修正する', ['class' => 'btn btn-primary']) !!}
-    {!! Form::button('キャンセル', ['class' => 'btn btn-secondary', 'onClick' => 'location.href="'.route('books.show', $book->id).'"']) !!}
-{!! Form::close() !!}
+        <div class="button-group">
+            {!! Form::submit('修正する', ['class' => 'button button-primary']) !!}
+            {!! Form::button('キャンセル', ['class' => 'button button-secondary', 'onClick' => 'location.href="'.route('books.show', $book->id).'"']) !!}
+        </div>
+    {!! Form::close() !!}
+</div>
 
 @endsection
